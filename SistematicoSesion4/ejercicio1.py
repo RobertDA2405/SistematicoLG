3a17467eb4c5901c93ad6935043ad8e028771f4e
robert24 <robertodavid2405@gmail.com>
2024-02-10 10:02:35 -0600

    Ejercicio 1

--- SistematicoSesion4/ejercicio1.py
+++ SistematicoSesion4/ejercicio1.py
@@ -6,7 +6,13 @@ class Rectangulo:
     def calcular(self):
         print(f"El area del rectangulo es {self.base*self.altura}")
 
+    def calculart(self):
+        print(f"El area del triangulo es {(self.base*self.altura)/2}")
 
 #Crear objeto calcular
 calculo1 = Rectangulo(5, 8)
 calculo1.calcular()
+
+#Triangulo
+triangulo = Rectangulo(2, 5)
+triangulo.calculart()
